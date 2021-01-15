7856443270d0c9f26583049038b182a6d356e228
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2021-01-15 21:15:59 +0200

    Added comments

--- main.py
+++ main.py
@@ -6,35 +6,42 @@ import sys
 
 
 def read_config_file(path):
+    """ Read config file line by line
+        The file is ordered according to this structure:
+        <#1>,<#2>,<#3>,<#4>
+        <vaccines>
+        <suppliers>
+        <clinics>
+        <logistics> """
     with open(path) as config_file:
         vaccines_count, suppliers_count, clinics_count, logistics_count = [0] * 4
         for ind, line in enumerate(config_file):
             curr_line = line.split(',')
-            if ind == 0:
+            if ind == 0:  # First line we get the counters for each type
                 vaccines_count, suppliers_count, clinics_count, logistics_count = list(map(int, curr_line))
             elif vaccines_count > 0:
                 vaccine = Vaccine(int(curr_line[0]), curr_line[1], int(curr_line[2]), int(curr_line[3]))
-                repo.vaccines.insert(vaccine)
+                repo.vaccines.insert(vaccine)  # Insert to appropriate table
                 vaccines_count -= 1
             elif suppliers_count > 0:
                 supplier = Supplier(int(curr_line[0]), curr_line[1], int(curr_line[2]))
-                repo.suppliers.insert(supplier)
+                repo.suppliers.insert(supplier)  # Insert to appropriate table
                 suppliers_count -= 1
             elif clinics_count > 0:
                 clinic = Clinic(int(curr_line[0]), curr_line[1], int(curr_line[2]), int(curr_line[3]))
-                repo.clinics.insert(clinic)
+                repo.clinics.insert(clinic)  # Insert to appropriate table
                 clinics_count -= 1
             elif logistics_count > 0:
                 logistic = Logistic(int(curr_line[0]), curr_line[1], int(curr_line[2]), int(curr_line[3]))
-                repo.logistics.insert(logistic)
+                repo.logistics.insert(logistic)  # Insert to appropriate table
                 logistics_count -= 1
             else:
                 continue
 
 
 def read_orders_file(orders_path, output_path):
-    with open(orders_path) as order_file:
-        for line in order_file:
+    with open(orders_path) as orders_file:
+        for line in orders_file:
             curr_line = line.split(',')
             if len(curr_line) == 3:  # Receive Shipment
                 print("Receive Shipment")
@@ -53,12 +60,18 @@ def read_orders_file(orders_path, output_path):
 
 
 def write_output_file(path, line_arg):
+    """
+        get output file path and add the line
+        <total_inventory>,<total_demand>,<total_received>,<total_sent>
+    """
     output_line = ','.join(list(map(str, line_arg))) + '\n'
     with open(path, 'a') as output_file:
         output_file.write(output_line)
 
 
 def update_output(path):
+    """ get the Summary of current state of totals and send
+        them to be updated in output file"""
     inventory = repo.vaccines.total_inventory()
     demand = repo.clinics.total_demand()
     received = repo.logistics.total_received()
@@ -70,12 +83,7 @@ if __name__ == '__main__':
     if len(sys.argv) < 4:
         print("Paths not supplied")
         pass
-    config_path, orders_path, output_path = sys.argv[1:]
-    repo.create_tables()
+    config_path, orders_path, output_path = sys.argv[1:]  # get files paths from arguments
+    repo.create_tables()  # create Vaccine, Supplier, Clinic, Logistic tables
     read_config_file(config_path)
     read_orders_file(orders_path, output_path)
-    # print(f"current: {repo.vaccines.find(1).quantity}")
-    # repo.vaccines.take(1, 20)
-    # print(f"current: {repo.vaccines.find(1).quantity}")
-    # repo.vaccines.add(1, 30)
-    # print(f"current: {repo.vaccines.find(1).quantity}")
